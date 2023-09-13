e9039c43d397d9d40a2a3f06e4abc50073c0d07f
Jigesh Sheoran <106379604+sheoraninfosec@users.noreply.github.com>
2023-09-13 22:19:18 +0530

    Create Day1.py
    Basic mathematics and checking data types

---
+++ Day 1 Introduction/Day1.py
@@ -0,0 +1,18 @@
+print(2+3)
+print(3-1)
+print(2*3)
+print(3/2)
+print(3**2)  # ** means exponential value
+print(3%2)   # % means modulus
+print(3//2)  # floor division operator
+
+#Checking Data Types
+
+print(type(10))                 # int
+print(type(3.14))               # float
+print(type(1 +3j))              # complex
+print(type('Jigesh'))           # string
+print(type([1,2,3]))            # list
+print(type({'name':'Jigesh'}))  # dictionary
+print(type({9.8, 3.14, 2.7}))   # set
+print(type((9.8, 3.14, 2.7)))   # tuple
